355b706a0c323ccfb9cdd71fc2ad9e76384815d6
Emerson Macedo <emerleite@gmail.com>
2012-05-07 12:01:58 -0300

    Implemented find, count and average methods

--- app/models/ratings.js
+++ app/models/ratings.js
@@ -5,15 +5,33 @@ client.select(process.env.REDIS_DB || "0");
 module.exports = {
   build : function(params) {
     return new Ratings(params);
+  },
+  find: function(slug, callback) {
+    var rating = new Ratings({slug: slug});
+
+    client.multi().
+      get(rating.slug()).
+      get(rating.slug_count()).
+      exec(function(err, replies) {
+        if (!replies[0] || !replies[1]) return callback(null, null);
+
+        var rating = new Ratings({
+          slug: slug,
+          rating: replies[0],
+          count: replies[1]
+        });
+        return callback(null, rating);
+      });
   }
 };
 
 function Ratings(params) {
-  console.log(params);
-
   var slug = params.slug;
   var slug_count = slug + ':count';
   var value = params.rating;
+  var count = parseInt(params.count, 10);
+
+  var maxValue = process.env.RATINGS_MAX_VALUE || null;
 
   return {
     slug: function() {
@@ -23,12 +41,19 @@ function Ratings(params) {
       return slug_count;
     },
     save: function(callback) {
+      if (value <= 0) return callback(new Error('rating must be greater than 0'));
+      if (maxValue && value > maxValue) return callback(new Error('rating must be at most ' + maxValue));
+
       client.multi().
         incrby(slug, value).
         incr(slug_count).
-        exec(function(err) {
-          return callback(err);
-        });
+        exec(callback);
+    },
+    average: function() {
+      return value / count;
+    },
+    count: function() {
+      return count;
     }
   }
 }
--- test/models/rating.test.js
+++ test/models/rating.test.js
@@ -26,7 +26,7 @@ describe('Ratings Model', function() {
   });
 
   describe('Ratings::build', function() {
-    it('deveria criar um objeto com os dados do rating', function(done) {
+    it('should create a Rating object with it data', function(done) {
       var rating = Ratings.build(params);
       rating.slug().should == 'some:slug';
       rating.slug_count().should == 'some:slug:count';
@@ -35,8 +35,8 @@ describe('Ratings Model', function() {
   });
 
   describe('Ratings#save', function() {
-    describe('com o banco vazio', function() {
-      it('deveria ter os valores totais iguais aos valores adicionados', function(done) {
+    describe('with empty database', function() {
+      it('should have rating and rating count as same as added', function(done) {
         var rating = Ratings.build(params);
         rating.save(function(err) {
           client.multi().
@@ -54,7 +54,7 @@ describe('Ratings Model', function() {
       });
     });
 
-    describe('com ratings já registrados', function() {
+    describe('with previous rating values', function() {
       var rating = Ratings.build(params);
 
       beforeEach(function(done) {
@@ -64,7 +64,7 @@ describe('Ratings Model', function() {
           exec(done);
       });
 
-      it('deveria somar os valores aos dados já existentes', function(done) {
+      it('should increment the values', function(done) {
         rating.save(function(err) {
           client.multi().
             get(rating.slug()).
@@ -79,6 +79,143 @@ describe('Ratings Model', function() {
             });
         });
       });
+    });
+
+    describe('with negative rating value' , function() {
+      var rating = Ratings.build({slug: 'some:slug', rating: -1});
+
+      it('should return with error', function(done) {
+        rating.save(function(err) {
+          should.exist(err);
+          err.message.should.equal('rating must be greater than 0');
+          done();
+        });
+      });
+    });
+
+    describe('when zero rating value', function() {
+      var rating = Ratings.build({slug: 'some:slug', rating: 0});
+
+      it('should return with error', function(done) {
+        rating.save(function(err) {
+          should.exist(err);
+          err.message.should.equal('rating must be greater than 0');
+          done();
+        });
+      });
+    });
+
+    describe('when value greater than max alowed', function() {
+      var rating = Ratings.build({slug: 'some:slug', rating: process.env.RATINGS_MAX_VALUE + 1});
+
+      it('should return with error', function(done) {
+        rating.save(function(err) {
+          should.exist(err);
+          err.message.should.equal('rating must be at most ' + process.env.RATINGS_MAX_VALUE);
+          done();
+        });
+      });
+    });
+
+    describe('when max value is not defined', function() {
+      before(function() {
+        this.maxValue = process.env.RATINGS_MAX_VALUE;
+        process.env.RATINGS_MAX_VALUE = null;
+        this.rating = Ratings.build({slug: 'some:slug', rating: 99999})
+      });
+
+      it('should allow any value', function(done) {
+        this.rating.save(function(err) {
+          should.not.exist(err);
+          done();
+        });
+      });
+
+      before(function() {
+        process.env.RATINGS_MAX_VALUE = this.maxValue;
+      });
+    });
+  });
+
+  describe('Ratings::find', function() {
+    describe('when record not exists yet', function() {
+      it('should return null', function(done) {
+        Ratings.find(params.slug, function(err, rating) {
+          should.not.exist(rating);
+          done();
+        });
+      });
+    });
+
+    describe('when record exists', function() {
+      var rating = Ratings.build(params);
+
+      beforeEach(function(done) {
+        client.multi().
+          incrby(rating.slug(), "8").
+          incrby(rating.slug_count(), "2").
+          exec(done);
+      });
+
+      it('should return an object with it values', function() {
+        Ratings.find(rating.slug(), function(err, existingRating) {
+          existingRating.slug().should.equal(rating.slug());
+          existingRating.slug_count().should.equal(rating.slug_count());
+        });
+      });
+    })
+  });
+
+  describe('Ratings#count', function() {
+    var rating = Ratings.build(params);
+    beforeEach(function(done) {
+      client.multi().
+        incrby(rating.slug(), "8").
+        incrby(rating.slug_count(), "2").
+        exec(done);
+    });
+
+    it('should retorn the rating count', function(done) {
+      Ratings.find(rating.slug(), function(err, existingRating) {
+        existingRating.count().should.equal(2);
+        done();
+      });
+    });
+  });
+
+  describe('Ratings#average', function() {
+    var rating = Ratings.build(params);
+
+    describe('when division without rest', function() {
+      beforeEach(function(done) {
+        client.multi().
+          incrby(rating.slug(), "8").
+          incrby(rating.slug_count(), "2").
+          exec(done);
+      });
+
+      it('should return an integer number', function(done) {
+        Ratings.find(rating.slug(), function(err, existingRating) {
+          existingRating.average().should.equal(4);
+          done();
+        });
+      })
+    });
+
+    describe('when division with rest', function() {
+      beforeEach(function(done) {
+        client.multi().
+          incrby(rating.slug(), "5").
+          incrby(rating.slug_count(), "2").
+          exec(done);
+      });
+
+      it('should return a decimal number', function(done) {
+        Ratings.find(rating.slug(), function(err, existingRating) {
+          existingRating.average().should.equal(2.5);
+          done();
+        });
+      })
     });
   });
 });
