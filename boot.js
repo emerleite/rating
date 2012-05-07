dba048173416f6ec50c59d96fa9f7fbb0902faf2
Emerson Macedo <emerleite@gmail.com>
2012-05-07 12:01:06 -0300

    Renamed app.js to server.js

--- boot.js
+++ boot.js
@@ -1,4 +1,4 @@
-var app = require('app');
+var server = require('server');
 var port = process.env.PORT || "3000";
 console.log('Server started at http://0.0.0.0:' + port);
-app.listen(port);
+server.listen(port);
